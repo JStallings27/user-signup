1cb6b5381fe8d9d79bcc5c09942e34741462a799
Jeremy <jeremystallings27@gmail.com>
2019-04-30 05:38:32 -0500

    Fully Functional

--- main.py
+++ main.py
@@ -1,42 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 import string
+import cgi
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-signup_form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>User Signup</h1>
-    <body>
-        <form method='POST'>
-            <label for=username/>Username</label>
-            <input type="text" name="user" id="username" value='{user}' />
-            <p class="error">{user_error}</p>
-
-  
-            <label for=password/>Password</label>
-            <input type="password" name="pass_word" id="password" value='{pass_word}' />
-            <p class="error">{pass_word_error}</p>
-
-            <label for=verify_password/>Verify Password</label>
-            <input type="password" name="verify" id="verify_password" value='{verify}'/>
-            <p class="error">{verify_error}</p>
-
-            <label for=email>Email (optional)</label>
-            <input type="text" name="email_address" id="email" value='{email_address}'/>
-            <p class="error">{email_address_error}</p>
-            
-            <input type="submit" value="Submit Query">
-        </form>
-    </body>
-"""
-
-@app.route("/")
+@app.route("/", methods=['POST'])
 def display_signup_form():
-    return signup_form.format(user='', user_error='', pass_word='',pass_word_error='',
+    return render_template('sign_up.html', user='', user_error='', pass_word='',pass_word_error='',
     verify='', verify_error='', email_address='', email_address_error='' )
 
 def acceptable(entry):
@@ -45,84 +17,112 @@ def acceptable(entry):
     'P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','?',
     '@','#','$','%','&','*','-','+','_','.']
     
-    try:
-        for x in entry:
-            if x in is_acceptable:
-                return True
-    except ValueError:
+    for x in entry:
+        if x in is_acceptable:
+            return True
+        else:
+            return False
+
+def val_length(length):
+    if len(length) > 2 and len(length) < 22:
+        return True
+    else:
+        return False
+
+def email_at_symbol(at):
+    if at.count('@') == 1:
+        return True
+    else:
+        return False
+
+def email_dot_com(dot_com):
+    if dot_com[-4:] == '.com':
+        return True
+    else:
         return False
 
 @app.route("/", methods=['POST'])
-def validation():
+def sign_validation():
     user = request.form['user']
-    user_error=''
-    email_address = request.form['email_address']
-    email_address_error=''
-
-
-    if len(user) < 3 or len(user) > 20:
-        user_error = 'Username must be 3 - 20 characters'
-    else:
-        user = user
-
-    if not acceptable(user):
-        user_error = 'Must only contain letters, numbers, and the available symbols(!?@#$%&*-+_.)'
-    else:
-        user = user
-
     pass_word = request.form['pass_word']
     verify = request.form['verify']
+    email_address = request.form['email_address']
+
+    user_error=''
     pass_word_error = ''
     verify_error = ''
-
+    email_address_error = ''
+###################################################################
     
-    if len(pass_word) < 3 or len(pass_word) > 20:
-        pass_word_error = 'Password must be 3 - 20 characters'
-        pass_word = ''
-        verify = ''
+    if not acceptable(user):
+        user_error = 'Must only contain letters, numbers, and the available symbols(!?@#$%&*-+_.)'
+    elif not val_length(user):
+        user_error = 'Username must be 3 - 21 characters'
     else:
-        pass_word = pass_word
-
+        user = user
+        user_error = ''
+###################################################################
+    
     if not acceptable(pass_word):
         pass_word_error = 'Must only contain letters, numbers, and the available symbols(!?@#$%&*-+_.)'
         pass_word = ''
         verify = ''
-    else:
-        pass_word = pass_word
-
-    if len(verify) < 3 or len(verify) > 20:
-        verify_error = 'Password must be 3 - 20 characters'
+    elif not val_length(pass_word):
+        pass_word_error = 'Username must be 3 - 21 characters'
         pass_word = ''
         verify = ''
     else:
-        verify = verify
+        pass_word = pass_word
+        pass_word_error = ''
+###################################################################
     
     if not acceptable(verify):
         verify_error = 'Must only contain letters, numbers, and the available symbols(!?@#$%&*-+_.)'
         pass_word = ''
         verify = ''
-    else:
-        verify = verify
-
-    if pass_word != verify:
-        pass_word_error = 'Both passwords must match'
-        verify = 'Both passwords must match'
+    elif not val_length(verify):
+        verify_error = 'Username must be 3 - 21 characters'
+        pass_word = ''
+        verify = ''
+    elif verify != pass_word:
+        pass_word_error = 'Passwords need to match'
+        verify_error = 'Passwords need to match'
         pass_word = ''
         verify = ''
     else:
-        pass_word = pass_word
         verify = verify
+        verify_error = ''
+###################################################################
+    
+    if len(email_address) > 0:
+        if not acceptable(email_address):
+            email_address_error = 'Must only contain letters, numbers, and the available symbols(!?@#$%&*-+_.)'
+        elif not val_length(email_address):
+            email_address_error = 'Username must be 3 - 21 characters'
+        elif not email_at_symbol(email_address):
+            email_address_error = 'Must contain one @ symbol'
+        elif not email_dot_com(email_address):
+            email_address_error = 'Must end with .com'
+        else:
+            email_address = email_address
+            email_address_error = ''
+    else:
+        email_address = ''
+        email_address_error = ''
+
+    
 
     if not user_error and not pass_word_error and not verify_error and not email_address_error:
-        return "Success"
+        return redirect("/welcome")
     else:
-        return signup_form.format(user=user, user_error=user_error, pass_word_error=pass_word_error, verify_error=verify_error, pass_word=pass_word,
-         verify=verify, email_address=email_address, email_address_error=email_address_error)
+        return render_template('sign_up.html', user=user, user_error=user_error, pass_word=pass_word, pass_word_error=pass_word_error, verify=verify, verify_error=verify_error, 
+        email_address=email_address, email_address_error=email_address_error)
 
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
-    return '<h1>Welcome</h1>'
+    verified = request.args.get('user')
+    return '<h1></h1>'
 
 
 app.run()
